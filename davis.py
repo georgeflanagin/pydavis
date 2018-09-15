b34ccd62318cee682b59d1bd56146e45fe562857
George Kelly Flanagin <me@georgeflanagin.com>
2018-09-15 15:54:08 -0400

    Deleted a vestigal line from the template.

--- davis.py
+++ davis.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Talk to a Davis Instruments weather station.
--- daviscommands.py
+++ daviscommands.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Give names to the Davis Instruments commands.
